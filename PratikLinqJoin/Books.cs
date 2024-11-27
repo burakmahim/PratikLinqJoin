04c1879665a97ec7bf9a161251b4a9225288734d
burakmahim <burakmahim718@gmail.com>
2024-11-27 12:10:55 +0300

    Proje dosyası ekle.

---
+++ PratikLinqJoin/Books.cs
@@ -0,0 +1,9 @@
+﻿namespace PratikLinqJoin
+{
+    public class Books
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public int AuthorId { get; set; }
+    }
+}
